eb6fa5f0e907fa94ad706d70cbdc9828e25ec60a
Cosmin-Gabriel Samoila <gabrielcsmo@gmail.com>
2019-05-25 01:13:45 +0300

    Working with Screen Analysis for HP/SP/MP
    Signed-off-by: Cosmin-Gabriel Samoila <gabrielcsmo@gmail.com>

--- FighterBot.py
+++ FighterBot.py
@@ -2,11 +2,13 @@ from Bot import Bot
 import keyboard as kb
 from time import sleep
 from random import randint
+from time import time
+from ImageProcessing import ImgProcessor
 
 class FighterBot(Bot):
 
-    MIN_ATTACKS = 5
-    MAX_ATTACKS = 6
+    MIN_ATTACKS = 3
+    MAX_ATTACKS = 5
 
     SPELLS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -22,19 +24,38 @@ class FighterBot(Bot):
     REBUFF_KEY = 'r'
     STOP_KEY = 'Ctrl'
 
-    NUM_BUFFS = 2
-    MIN_SPELL_DELAY = 150 # /100 to get number of seconds
-    MAX_SPELL_DELAY = 250 # /100 to get number of seconds
+    NUM_BUFFS = 5
+    MIN_SPELL_DELAY = 120 # /100 to get number of seconds
+    MAX_SPELL_DELAY = 150 # /100 to get number of seconds
 
     JUMP_PROBABILITY = -1 # in %
     REST_TIME = 15 # in seconds
-    REST_THRESHOLD = 1000 # in number of mobs killed
+    REBUFF = 50 # in number of mobs killed
+    POINTS_THRESHOLD = 45 # in %
 
-    def __init__(self):
+    def __init__(self, resolution=(1920, 1080)):
         Bot.__init__(self)
         self.name = "mage_bot"
         self.killed_mobs = 0
         self.total_killed = 0
+        self.resolution = resolution
+        self.screen_hdl = ImgProcessor(resolution)
+        self.hp = 100
+        self.mp = 1003
+        self.sp = 100
+        self.pot_type = 0
+        self.pots = ['x', 'z']
+
+    def pot(self):
+        if self.hp < FighterBot.POINTS_THRESHOLD:
+            kb.press_and_release(self.pots[self.pot_type % 2])
+            print("HP {}% - poting".format(self.hp))
+            self.pot_type += 1
+            sleep(0.5)
+        if self.sp < FighterBot.POINTS_THRESHOLD:
+            print("SP {}% or MP {}% - poting".format(self.sp, self.mp))
+            kb.press_and_release("z")
+            sleep(0.5)
 
     def rebuff(self):
         for i in xrange(FighterBot.NUM_BUFFS):
@@ -57,7 +78,7 @@ class FighterBot(Bot):
     def attack(self):
         num_attacks = randint(FighterBot.MIN_ATTACKS, FighterBot.MAX_ATTACKS)
         #print("Attacking {} times".format(num_attacks))
-        print("Monster {}.".format(self.killed_mobs))
+        print("[{0:.2f}] Monster {1}.".format(self.current_time, self.killed_mobs))
         for i in xrange(num_attacks):
             # jump with a givenprobability
             #if randint(0, 100) < FighterBot.JUMP_PROBABILITY:
@@ -67,8 +88,11 @@ class FighterBot(Bot):
                 kb.press_and_release(FighterBot.SPELLS[0])
             elif i == 1:
                 kb.press_and_release(FighterBot.SPELLS[1])
-            else:
+            elif i == 2:
                 kb.press_and_release(FighterBot.SPELLS[2])
+            else:
+                kb.press_and_release(FighterBot.SPELLS[3])
+            
             delay = randint(FighterBot.MIN_SPELL_DELAY, FighterBot.MAX_SPELL_DELAY) / 100.0
             #print("Wait {} until next attack".format(delay))
             sleep(delay)
@@ -83,29 +107,39 @@ class FighterBot(Bot):
         sleep(FighterBot.REST_TIME)
         kb.press_and_release(FighterBot.FW_KEY)
 
+    def update_status(self):
+        self.screen_hdl.get_resources_status()
+        self.hp = self.screen_hdl.get_res("HP", "1")
+        self.mp = self.screen_hdl.get_res("MP", "1")
+        self.sp = self.screen_hdl.get_res("SP", "1")
+
     def main_loop(self):
+        self.start_time = time()
+        self.current_time = 0
+        print self.start_time
+        pots = ['z', 'x']
         print("Waiting {} key to start. Stop it using the same key".format(FighterBot.STOP_KEY))
+
         kb.wait(FighterBot.STOP_KEY)
-
-        for i in range(5):
-            print("Starting in {} sec...".format(5 - i))
-            sleep(1)
-
-        #self.rebuff()
+        sleep(2)
 
         self.killed_mobs = 0
         while True:
+            self.current_time = time() - self.start_time
             # exit key
             if kb.is_pressed(FighterBot.STOP_KEY):
                 print("Killed ~ {} mobs".format(self.total_killed + self.killed_mobs))
                 break
 
+            if self.current_time > 800:
+                self.rebuff()
+                self.current_time = 0
+
+            # get HP, SP, MP
+            self.update_status()
+
+            # pot if necessary
+            self.pot()
+
             self.select_next_monster()
             self.attack()
-
-            if self.killed_mobs % 10 == 0:
-                kb.press_and_release('z')
-            if self.killed_mobs == FighterBot.REST_THRESHOLD:
-                self.rest()
-                self.total_killed += self.killed_mobs
-                self.killed_mobs = 0
--- ImageProcessing.py
+++ ImageProcessing.py
@@ -13,32 +13,43 @@ class ImgProcessor():
     TEXT_THRESHOLD = 150
 
     fragments = {
-        "COORDINATES" : (35, 70, 220, 100)
+        "HP1" : (21, 120, 139, 121),
+        "MP1" : (21, 125, 139, 126),
+        "SP1" : (21, 135, 139, 136)
     }
 
     def __init__(self, resolution=(1920,1080)):
         print("Image Processing module initialized")
         self.resolution = resolution
         self.coordinates = []
+        self.boxes = {}
 
     def grab_frame(self, box):
         im = ImageGrab.grab(bbox=box)
         im2 = np.asanyarray(im)
         screen = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-        for i in range(len(screen)):
-            for j in range(len(screen[i])):
-                if screen[i][j] < ImgProcessor.TEXT_THRESHOLD:
-                    screen[i][j] = 0
+        #screen = cv2.cvtColor(im2, cv2.COLOR_BGR2RGB)
+        return screen
 
-        im2 = cv2.threshold(screen, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        return im2
-
-    def display_frame(self):
-
+    def get_resources_status(self):
+        """get all 3 type"""
         for key in ImgProcessor.fragments:
-            screen = self.grab_frame(ImgProcessor.fragments[key])
-            text = image_to_string(screen)
-            cv2.imshow(key, cv2.cvtColor(screen, cv2.COLOR_GRAY2RGB))
-            print(text)
+            self.boxes[key] = self.grab_frame(ImgProcessor.fragments[key])
+
+    def get_res(self, res="HP", player = "1"):
+        if "{}{}".format(res, player) not in self.boxes:
+            print("Missing {} box for player {}".format(res, player))
+            return None
+        row = self.boxes["{}{}".format(res, player)]
+        count = 0
+        for i in row[0]:
+            if i > 10:
+                count += 1
+        return count * 100 / len(row[0])
+
+    def display_frame(self, box_name="HP1"):
+        screen = self.grab_frame(self.fragments[box_name])
+        cv2.imshow(box_name, cv2.cvtColor(screen, cv2.COLOR_GRAY2RGB))
+        #cv2.imshow(box_name, cv2.cvtColor(screen, cv2.COLOR_RGB2BGR))
         cv2.waitKey(0)
         cv2.destroyAllWindows()
